c76caa9e8b3becfed396c95a6bad5992463414c4
Tioga060 <jeffhanford060@gmail.com>
2019-04-11 22:01:51 -0500

    better search

--- src/Page/components/TopBar/TopBar.tsx
+++ src/Page/components/TopBar/TopBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createRef } from 'react';
 import TopAppBar, {
     TopAppBarFixedAdjust,
     TopAppBarIcon,
@@ -8,6 +8,7 @@ import TopAppBar, {
 } from "@material/react-top-app-bar";
 import MaterialIcon from '@material/react-material-icon';
 import TextField, { Input } from '@material/react-text-field';
+import debounce from 'lodash/debounce';
 import {
     Headline6,
     Subtitle2,
@@ -21,48 +22,86 @@ import './styles.scss';
 interface IState {
     searchText: string;
     searchIsOpen: boolean;
+    queryText: string;
 }
 
 interface IBlankProps { }
 
 export class TopBar extends React.Component<IBlankProps, IState> {
+    public searchInputRef: React.RefObject<Input>;
+
     public constructor(props: IBlankProps) {
         super(props);
         this.state = {
             searchText: '',
             searchIsOpen: false,
+            queryText: '',
         };
+        this.updateQuery = debounce(this.updateQuery, 250);
+        this.keyEvent = this.keyEvent.bind(this);
+        this.searchInputRef = createRef();
+    }
+
+    public keyEvent(event: any) {
+        if (event.keyCode === 27) { // Escape
+            this.closedSearch();
+        }
+    }
+
+    public componentDidMount() {
+        document.addEventListener("keydown", this.keyEvent, false);
+    }
+
+    public componentWillUnmount() {
+        document.removeEventListener("keydown", this.keyEvent, false);
+    }
+
+    public setSearchText = (e: React.FormEvent<HTMLInputElement>) => {
+        this.setState({ searchText: e.currentTarget.value })
+        this.updateQuery(e.currentTarget.value);
+    }
+
+    public updateQuery = (query: string) => {
+        this.setState({ queryText: query });
+    }
+
+    public clickedSearch = () => {
+        this.setState(
+            () => ({ searchIsOpen: true })
+        )
+    }
+
+    public closedSearch = () => {
+        this.setState(() => ({
+            searchIsOpen: false,
+            searchText: '',
+        }))
+        this.updateQuery('');
     }
 
     public render() {
         return (
             <>
-                <TopAppBar>
+                <TopAppBar className="top-app-bar">
                     <TopAppBarRow>
                         {this.state.searchIsOpen
                             ? (
-                                <>
-                                    <TopAppBarSection align='end' role='toolbar'>
-                                        <TextField
-                                            label='Map name, Author ...'
-                                            className='search-box mdc-text-field__input mdc-text-field--fullwidth'
-                                            onTrailingIconSelect={() => this.setState({ searchText: '' })}
-                                            trailingIcon={<MaterialIcon role="button" icon="search" />}
-                                            fullWidth
-                                        ><Input
-                                                value={this.state.searchText}
-                                                onChange={(e: any) => this.setState({ searchText: e.currentTarget.value })} />
-                                        </TextField>
-                                        <TopAppBarIcon actionItem tabIndex={0}>
-                                            <MaterialIcon
-                                                aria-label="close"
-                                                hasRipple
-                                                icon='close'
-                                                onClick={() => this.setState({ searchIsOpen: false })}
-                                            />
-                                        </TopAppBarIcon>
-                                    </TopAppBarSection>
-                                </>
+                                <TopAppBarSection align='end' role='toolbar'>
+                                    <TextField
+                                        label='Map name, Author ...'
+                                        className='search-box mdc-text-field__input mdc-text-field--fullwidth'
+                                        onTrailingIconSelect={this.closedSearch}
+                                        trailingIcon={<MaterialIcon role="button" icon="close" className="search-box-icon" />}
+                                        fullWidth
+                                    >
+                                        <Input
+                                            autoFocus
+                                            value={this.state.searchText}
+                                            onChange={this.setSearchText}
+                                        />
+                                    </TextField>
+
+                                </TopAppBarSection>
                             )
                             : (<>
                                 <TopAppBarSection align='start'>
@@ -74,10 +113,10 @@ export class TopBar extends React.Component<IBlankProps, IState> {
                                 <TopAppBarSection align='end' role='toolbar'>
                                     <TopAppBarIcon actionItem tabIndex={0}>
                                         <MaterialIcon
-                                            aria-label="search"
+                                            aria-label="close"
                                             hasRipple
                                             icon='search'
-                                            onClick={() => this.setState({ searchIsOpen: true })}
+                                            onClick={this.clickedSearch}
                                         />
                                     </TopAppBarIcon>
                                 </TopAppBarSection>
@@ -90,7 +129,7 @@ export class TopBar extends React.Component<IBlankProps, IState> {
                 <TopAppBarFixedAdjust>
                     <div className="App-body">
                         <MapList
-                            searchText={this.state.searchText}
+                            searchText={this.state.queryText}
                         />
                     </div>
                 </TopAppBarFixedAdjust>
